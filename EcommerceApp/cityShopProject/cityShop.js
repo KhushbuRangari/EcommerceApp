897fd0e98943f8719c676ef1a261f47730491446
Khushbu Rangari <126225826+KhushbuRangari@users.noreply.github.com>
2023-08-02 15:17:54 +0530

    Delete EcommerceApp/cityShopProject directory

--- EcommerceApp/cityShopProject/cityShop.js
+++
@@ -1,8 +0,0 @@
-let shoppingCart = document.querySelector('.shopping-cart')
-
-console.log(shoppingCart);
-
-
-document.querySelector('#cart-btn').onclick=()=>{
-    shoppingCart.classList.toggle('active');
-} 

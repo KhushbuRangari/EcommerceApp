f0ea27c66b8130b2bd5f702a15d9e0e2086c1b19
Khushbu Rangari <khushbu.rangari@gmail.com>
2023-08-07 21:01:28 +0530

    added dropdown selection based filter and show product

--- cityShopProject/cityShop.js
+++ cityShopProject/cityShop.js
@@ -6,8 +6,12 @@ document.querySelector('#cart-btn').onclick=()=>{
 
 let content = document.querySelector(".content");
 let showProductList = document.querySelector("#list1");
+let showCategryProduct = document.querySelector("#categoryProduct");
 let carousel1 = document.querySelector("#carousel1")
-let category = document.querySelector(".dropdown-menu")
+let category = document.querySelector(".custom-select")
+let categorySelect = document.getElementById('#select')
+
+
 let carousel2 = document.querySelector("#carousel2");
 var dataGlobal = [];
 var categoryData = [];
@@ -27,21 +31,26 @@ async function getData() {
      categoryData = newar.filter((ele, i, ar) => { return ar.indexOf(ele) === i })
     //  console.log(categoryData);
 
-    //----------load carausel 2-------------
-    console.log(dataGlobal[0].images[0]);
+    //----------load carausel 2------------- 
     loadcarousel2();
+
+    //-----------load category  -------------
+    loadCategory();
+
 }
 async function fetchproducts() {
     content.innerHTML = '';
     carousel1.innerHTML ='';
    
     dataGlobal.map((element)=>{
-        showProductList.innerHTML += `<div class="card " style="font-size: 10px;width: 240px; height: 280px;
+        showProductList.innerHTML += `<div class="card " style="font-size: 10px;width: 240px; height: 350px;
          margin: 6px; border: 2px solid  rgb(21, 37, 82)  box-shadow: 10px 3px 15px #464444;" id="${element.title}">
         <img src="${element.thumbnail}" style="height: 100px; width: 100%" class="card-img-top" alt="...">
         <div class="card-body align-self-center" >
           <h5 class="card-title" >${element.title}</h5>
           <p class="card-text" >${element.description}</p>
+          <p class="card-text" style="font-weight:bold" >Price: ${element.price}</p>
+        
         </div>
         <a href="#" class="btn btn-outline-primary">Add to Cart</a>
       </div>
@@ -52,12 +61,42 @@ carousel2.innerHTML += ``
 
 }
 async function loadCategory() {
-    category.innerHTML='';
+    // category.innerHTML='';
     categoryData.forEach(element => {
-        category.innerHTML += ` 
-    <li><a class="dropdown-item" href="#">${element}</a></li>
-  `
-    });
+        category.innerHTML += `<option class="dropdown-item" value="${element}" >${element}</option>`
+   });
+
+}
+
+
+
+ function selectOption(){
+    let selected = category.value;
+    content.innerHTML = '';
+    carousel1.innerHTML ='';
+    showProductList.innerHTML='';
+     
+ 
+
+    for (let i = 0; i < dataGlobal.length; i++) {
+        const element = dataGlobal[i].category;
+            if(element===selected){
+               
+                    showProductList.innerHTML += `<div class="card " style="font-size: 10px;width: 240px; height: 350px;
+                     margin: 6px; border: 2px solid  rgb(21, 37, 82)  box-shadow: 10px 3px 15px #464444;" id="${dataGlobal[i].title}">
+                    <img src="${dataGlobal[i].thumbnail}" style="height: 100px; width: 100%" class="card-img-top" alt="...">
+                    <div class="card-body align-self-center" >
+                      <h5 class="card-title" >${dataGlobal[i].title}</h5>
+                      <p class="card-text" >${dataGlobal[i].description}</p>
+                      <p class="card-text" style="font-weight:bold" >Price: ${dataGlobal[i].price}</p>
+                 
+                    </div>
+                    <a href="#" class="btn btn-outline-primary">Add to Cart</a>
+                  </div>
+            `
+                
+            }
+      }
 
 }
 
@@ -70,7 +109,7 @@ let randomNo3 = Math.floor(Math.random()*30)
 
 async function loadcarousel2(){
 
-  //  dataGlobal.map((element)=>{
+ 
             let element = dataGlobal[randomNo1]
             let element2 = dataGlobal[randomNo2]
             let element3 = dataGlobal[randomNo3]
@@ -163,21 +202,7 @@ async function loadcarousel2(){
   </div>
 
 `
-   // }
-    
-  //  )
-    
 
-
-    // dataGlobal.forEach(element=>{
-    //     console.log(element);
-    //     for (let i = 0; i < 4; i++) {
-    //         carousel2.innerHTML +=`<img
-    //         src="${element.images[i]}"
-    //         class="card-img-top" alt="Project 4">`
-            
-    //     }
-    // })
 }
 
 

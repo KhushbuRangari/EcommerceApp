44a149fe22e3b82bf838eb9cd980c6d0b83a969c
Khushbu Rangari <khushbu.rangari@gmail.com>
2023-08-07 12:14:22 +0530

    added second carousel---working

--- cityShopProject/cityShop.js
+++ cityShopProject/cityShop.js
@@ -8,6 +8,7 @@ let content = document.querySelector(".content");
 let showProductList = document.querySelector("#list1");
 let carousel1 = document.querySelector("#carousel1")
 let category = document.querySelector(".dropdown-menu")
+let carousel2 = document.querySelector("#carousel2");
 var dataGlobal = [];
 var categoryData = [];
 
@@ -16,7 +17,7 @@ async function getData() {
     const data = await response.json();
     dataGlobal =data.products;
     const newar = [];
-
+    
     //----get distinct category ----
     for (let i = 0; i < dataGlobal.length; i++) {
         const element = dataGlobal[i].category;
@@ -25,10 +26,15 @@ async function getData() {
      // -- distinct category --
      categoryData = newar.filter((ele, i, ar) => { return ar.indexOf(ele) === i })
     //  console.log(categoryData);
+
+    //----------load carausel 2-------------
+    console.log(dataGlobal[0].images[0]);
+    loadcarousel2();
 }
 async function fetchproducts() {
     content.innerHTML = '';
     carousel1.innerHTML ='';
+   
     dataGlobal.map((element)=>{
         showProductList.innerHTML += `<div class="card " style="font-size: 10px;width: 240px; height: 280px;
          margin: 6px; border: 2px solid  rgb(21, 37, 82)  box-shadow: 10px 3px 15px #464444;" id="${element.title}">
@@ -41,6 +47,9 @@ async function fetchproducts() {
       </div>
 `
     })
+
+carousel2.innerHTML += ``
+
 }
 async function loadCategory() {
     category.innerHTML='';
@@ -51,6 +60,130 @@ async function loadCategory() {
     });
 
 }
+
+
+let randomNo1 = Math.floor(Math.random()*30)
+let randomNo2 = Math.floor(Math.random()*30)
+let randomNo3 = Math.floor(Math.random()*30)
+
+
+
+async function loadcarousel2(){
+
+  //  dataGlobal.map((element)=>{
+            let element = dataGlobal[randomNo1]
+            let element2 = dataGlobal[randomNo2]
+            let element3 = dataGlobal[randomNo3]
+         console.log(randomNo1,'cara2');
+
+
+        carousel2.innerHTML += `   <div class="carousel-item active">
+        <div class="row row-cols-1 row-cols-md-3 g-4"  >
+          <div class="col">
+            <div class="card" >
+              <img
+                src="${element.images[0]}"
+                class="card-img-top" alt="Project 4">
+           
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img
+                src="${element.images[1]}"
+                class="card-img-top" alt="Project 5">
+            
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" >
+              <img
+                src="${element.images[2]}"
+                class="card-img-top" alt="Project 6">
+           
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item ">
+      <div class="row row-cols-1 row-cols-md-3 g-4"  >
+        <div class="col">
+          <div class="card" >
+            <img
+              src="${element2.images[0]}"
+              class="card-img-top" alt="Project 4">
+         
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img
+              src="${element2.images[1]}"
+              class="card-img-top" alt="Project 5">
+          
+          </div>
+        </div>
+        <div class="col">
+          <div class="card" >
+            <img
+              src="${element2.images[2]}"
+              class="card-img-top" alt="Project 6">
+         
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item ">
+    <div class="row row-cols-1 row-cols-md-3 g-4"  >
+      <div class="col">
+        <div class="card" >
+          <img
+            src="${element3.images[0]}"
+            class="card-img-top" alt="Project 4">
+       
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <img
+            src="${element3.images[1]}"
+            class="card-img-top" alt="Project 5">
+        
+        </div>
+      </div>
+      <div class="col">
+        <div class="card" >
+          <img
+            src="${element3.images[2]}"
+            class="card-img-top" alt="Project 6">
+       
+        </div>
+      </div>
+    </div>
+  </div>
+
+`
+   // }
+    
+  //  )
+    
+
+
+    // dataGlobal.forEach(element=>{
+    //     console.log(element);
+    //     for (let i = 0; i < 4; i++) {
+    //         carousel2.innerHTML +=`<img
+    //         src="${element.images[i]}"
+    //         class="card-img-top" alt="Project 4">`
+            
+    //     }
+    // })
+}
+
+
+
+
+
 
 
 
